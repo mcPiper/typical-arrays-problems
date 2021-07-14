77bb32473554897e5d6f6609d06230787116fa60
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-07-14 17:47:02 +0300

    init: upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length>0){
+        if (array.length>0) {
+          return Math.min.apply(null,array);
+        }
+      }
+      return 0;
+    
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length>0){
+        if (array.length>0) {
+          return Math.max.apply(null,array);
+        }
+      }
+      return 0;    
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+    if (arguments.length>0){
+        if (array.length>0) {
+          let sum=0;
+          for (let i=0; i<array.length;i++){
+            sum+=array[i];
+          }
+          return sum/array.length;
+        }
+      } 
+      return 0;
+    
 }

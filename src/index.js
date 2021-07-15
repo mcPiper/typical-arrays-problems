81cba495950f816047caea96504ad543d75279c1
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-07-15 14:08:13 +0300

    init: comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-exports.min = function min (array) {
+exptap.min = function min (array) {
     if (arguments.length>0){
         if (array.length>0) {
           return Math.min.apply(null,array);
@@ -9,20 +9,20 @@ exports.min = function min (array) {
     
 }
 
-exports.max = function max (array) {
+exptap.max = function max (array) {
     if (arguments.length>0){
         if (array.length>0) {
-          return Math.max.apply(null,array);
+          return Math.max.apply(null,array);  /* apply добавляет переменную в функцию. вся запись шаблон наиболее простого решения поиска в массиве больше меньше значения */
         }
       }
       return 0;    
 }
 
-exports.avg = function avg (array) {  
+exptap.avg = function avg (array) {  
     if (arguments.length>0){
         if (array.length>0) {
           let sum=0;
-          for (let i=0; i<array.length;i++){
+          for (let i=0; i<array.length;i++){     /* сложение всех элементов массива */
             sum+=array[i];
           }
           return sum/array.length;
